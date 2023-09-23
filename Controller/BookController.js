0799331dee62fc23a615308763965c30c2046543
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-09-23 21:27:12 +0530

    complete backend of the bookstore app

--- Controller/BookController.js
+++ Controller/BookController.js
@@ -1,25 +1,25 @@
 
 const Book = require('../Model/Book');
 
-const AllBooks= async(req,res,next)=>{
+const AllBooks = async (req, res, next) => {
     // this is the route that will help us to get all the books from the data base. 
     let books;
-    try{
-         books = await Book.find();
-    }catch(err){
-     console.log(err);
+    try {
+        books = await Book.find();
+    } catch (err) {
+        console.log(err);
     }
-    if(!books) {return res.status(404).json({message:'No books found'})}
+    if (!books) { return res.status(404).json({ message: 'No books found' }) }
     console.log("received request in getting all the books.");
-    return res.status(200).json({books});
- }
+    return res.status(200).json({ books });
+}
 
-const addBook = async(req,res,next)=>{
+const addBook = async (req, res, next) => {
     let book;
-    const {name,author,price,available,description}=req.body;
-    console.log("received a data of name: ", name,", author: ",author,", price: ",price, ", available: ",available );
-    try{
-        book= new Book({
+    const { name, author, price, available, description } = req.body;
+    console.log("received a data of name: ", name, ", author: ", author, ", price: ", price, ", available: ", available);
+    try {
+        book = new Book({
             name,
             author,
             price,
@@ -27,14 +27,56 @@ const addBook = async(req,res,next)=>{
             available
         })
         await book.save();
-
-    }catch(err){
-        console.log(err);
-        if(!book){
-            res.status(500).json({message:'Unable to Add'})
-        }    
-        return res.status(201).json({book})
+    } catch (err) {
+        console.log(err.message);
+        const message= err.message;
+        return res.status(500).json({ message });
     }
-
+    
+    return res.status(201).json({ "added to database ": book })
 };
-module.exports={addBook,AllBooks};
+
+const getByID = async (req,res,next)=>{
+    let book;
+    try{
+    book = await Book.findById(req.params.id);
+    }catch(err){
+        return  res.status(500).json(err.message);
+    }
+    console.log('sending the book with the id', req.params.id, 'to the client');
+    return res.status(201).json({book});
+}
+
+const updateById = async(req,res,next)=>{
+    console.log("reached the expected function");
+    const {name,author,price,description,available} = req.body;
+ try{
+    await Book.findByIdAndUpdate(req.params.id,{name,
+        author,
+        price,
+        description,
+        available});
+ }catch(err){
+    console.log('I was here');
+    return res.status(500).json(err.message);
+ }
+ return res.status(201).json({message:`book with id${req.id} has been updated.`});
+}
+
+const deleteById = async(req,res,next)=>{
+    try {
+        const idToRemove = req.params.id; // Extract the id from req.params
+        const book = await Book.findByIdAndRemove(idToRemove, {});
+
+        if (!book) {
+            return res.status(404).json({ message: 'Unable to find the book with this id' });
+        }
+
+        return res.status(200).json({ message: 'Deleted successfully' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: 'An error occurred', errorname: err.message });
+    }
+}
+
+module.exports = { addBook, AllBooks, getByID,updateById,deleteById };
--- Routes/BookStoreRoutes.js
+++ Routes/BookStoreRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {addBook,AllBooks}= require('../Controller/BookController');
+const {addBook,AllBooks, getByID,updateById,deleteById}= require('../Controller/BookController');
 
 router.get('/AllBooks',AllBooks);
 router.post('/addBook',addBook);
+router.get('/getBookbyID/:id',getByID);
+router.put('/updateById/:id',updateById);
+router.delete('/deleteById/:id', deleteById);
 module.exports = router;
