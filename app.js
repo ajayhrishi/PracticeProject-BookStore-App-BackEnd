0531e5f7004eeb664b0d1e5be48732c4bf3ee41a
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-02-25 04:22:54 +0530

    fixed issue with not getting updated

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require('express'); // copying the data of the express to the express variable
-const mongoose = require('mongoose'); //copying the mongoose to the mongoose variable
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import the CORS middleware
 const router = require('../book-store-backend/Routes/BookStoreRoutes');
-const app = express(); //adding the functionalities of the express to the app variable
+const app = express();
+
+// Use the CORS middleware to allow requests from all origins (*)
+app.use(cors());
+
 app.use(express.json());
-// connecting to the database 
-mongoose.connect('mongodb+srv://admin:fRYpKr63E9GAbfKr@cluster0.1ihedfg.mongodb.net/?retryWrites=true&w=majority')
-.then(()=>console.log('connected to the database'))
-.then(()=>app.listen(5000)).catch((err)=>console.log("Failed to connect with the database",err));
 
+mongoose
+  .connect(
+    'mongodb+srv://admin:A100125@cluster0.1ihedfg.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('Connected to the database'))
+  .then(() => app.listen(5000))
+  .catch((err) => console.log('Failed to connect with the database', err));
 
-
-app.use("/",router); // added a middleware to give a default response to the route. 
+app.use('/', router);   
