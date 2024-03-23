bb58c542174ed856b56133ca16d4e1c46a185f1e
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-03-24 02:21:38 +0530

    intial state to host the backend

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Import the CORS middleware
 const router = require('../book-store-backend/Routes/BookStoreRoutes');
-const app = express();
+dotenv.config();
 
+const app = express();
+const db = process.env.DB
 // Use the CORS middleware to allow requests from all origins (*)
 app.use(cors());
 
 app.use(express.json());
 
 mongoose
-  .connect(
-    'mongodb+srv://admin:A100125@cluster0.1ihedfg.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect(db)
   .then(() => console.log('Connected to the database'))
   .then(() => app.listen(5000))
   .catch((err) => console.log('Failed to connect with the database', err));
